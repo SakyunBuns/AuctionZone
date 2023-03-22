89856f1b5897688df6df50b9184ac2b18f56e55d
ArsenTigor <quoc.huan.tran26@gmail.com>
2023-03-22 16:14:02 -0400

    Moving component/Navbar.jsx to src temporary because of update page bug Color palette integration (use for dark mode later)

--- dev/src/App.jsx
+++ dev/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './css/App.css'
 import HomePage from './HomePage'
 import ProfilePage from './ProfilePage'
 import AuctionPage from './AuctionPage'
-import NavBar from './component/navbar'
+import NavBar from './Navbar'
+
 
 export default function App() {
+
+    const [darkMode, setDarkMode] = useState(false)
 
     const pages = [
         {
@@ -29,6 +33,28 @@ export default function App() {
         }
     ]
 
+    const palette1 = {
+        color1: '#f5f5f5',
+        color2: '#e0e0e0',
+        color3: '#bdbdbd',
+        textColor: '#000000',
+    }
+    
+    const palette2 = {
+        color1: '#000000',
+        color2: '#212121',
+        color3: '#424242',
+        textColor: '#ffffff',
+    }
+
+    const [palette, setPalette] = useState(palette1)
+    
+    
+    function handleDarkMode(){
+        setDarkMode(!darkMode)
+        darkMode ? setPalette(palette1) : setPalette(palette2)
+    }
+
     const navbarRoutes = pages.map((page) => {
         return (
         <Route path={page.path} element={page.element} key={page.id}/>
@@ -36,12 +62,13 @@ export default function App() {
     })
 
     return (
-        <>
-        <NavBar pages={pages}/>
-        <Routes>
-            {navbarRoutes}
-            <Route path='*' element={<h1>error 404</h1>} key="0"/>
-        </Routes>
-        </>
+        <div>
+            <NavBar pages={pages} palette={palette}/>
+            <Routes>
+                {navbarRoutes}
+                <Route path='*' element={<h1>error 404</h1>} key="0"/>
+            </Routes>
+            <button onClick={handleDarkMode}>HERE</button>
+        </div>
     );
 }
---
+++ dev/src/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function NavBar(props){
+
+  
+  const styleContainer = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: `${props.palette.color1}`,
+    height: '50px',
+  }
+  
+  const styleLink = {
+    border: '2px solid black',
+    backgroundColor: `${props.palette.color2}`,
+    padding: '10px',
+    borderRadius: '3px',
+    color: `${props.palette.textColor}`,
+  }
+
+  const links = props.pages.map((page) => {
+    return (
+      <Link to={page.path} key={page.id}>
+        <div style={styleLink}> 
+          {page.name}
+        </div>
+      </Link>
+    )
+  })
+
+  return(
+      <div style={styleContainer}>
+          {links}
+      </div>
+  );
+}
