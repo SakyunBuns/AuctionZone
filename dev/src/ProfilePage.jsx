f4bc854e02a9b3682398e82b88acaa7d4c87b1b8
Nathaelle Fournier <nathaellefournier@gmail.com>
2023-03-23 15:57:38 -0400

    ajout script.ssh

--- dev/src/ProfilePage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ProfilePage(){
-    return(
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Welchcorgipembroke.JPG/1024px-Welchcorgipembroke.JPG" alt="" />
-    )
-}
