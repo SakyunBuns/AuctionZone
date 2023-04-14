33073516f422331cfb58707faaeb82692a78a042
Nathaelle Fournier <nathaellefournier@gmail.com>
2023-04-14 11:47:11 -0400

    Ajout fonction dao pour verification email

--- dev/app/src/DAO/UserDAO.js
+++ dev/app/src/DAO/UserDAO.js
@@ -1,23 +1,44 @@
 export class UserDAO {
-  static does_user_exist = (username) => {
+  static does_user_exist_username = (username) => {
     if (username != null) {
       fetch('http://127.0.0.1:3000/users_name/' + username, {
         method: 'GET',
       })
       .then(response => response.json())
-      .then(data => {
-        console.log(data); // log the data returned by the API
-        if (data != []) {
-          console.log('allo');
-          return true;
-        }
-        else {
-          return false;
-        }
-      })
-      .catch(error => {
-        console.error(error); // log any errors that occur during the request
-      });
+        .then(data => {
+          console.log(data); // log the data returned by the API
+          if (data != []) {
+            return true;
+          }
+          else {
+            return false;
+          }
+        })
+        .catch(error => {
+          console.error(error); // log any errors that occur during the request
+        });
     }
   }
+
+  static does_user_exist_email = (email) => {
+    if (email != null) {
+      fetch('http://127.0.0.1:3000/user_email/' + email, {
+        method: 'GET',
+      })
+      .then(response => response.json())
+        .then(data => {
+          console.log(data); // log the data returned by the API
+          if (data != []) {
+            return true;
+          }
+          else {
+            return false;
+          }
+        })
+        .catch(error => {
+          console.error(error); // log any errors that occur during the request
+        });
+    }
+  } 
+  
 }
