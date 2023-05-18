9d42af86b06a5a58b1f66f1de68504bef8232ae5
QHT <quoc.huan.tran@gmail.com>
2023-05-18 17:08:02 -0400

    Item section to take item from DB

--- dev/app/src/HomePage.jsx
+++ dev/app/src/HomePage.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import ItemSection from './component/ItemSection';
 import { UserDAO } from './DAO/UserDAO';
 import QuickSort from './assets/QuickSort';
+import { userContext } from './component/Context';
 
 export default function HomePage(props) {
 
@@ -10,24 +11,35 @@ export default function HomePage(props) {
         'USER_ONLINE': <ItemSection sectionName={'User recommendation'} sectionLink={'https://www.google.com'} bottomSpacing={20} containerHeight={300} imageSize={200} />
     }
     
-    const [userLoggedIn, setUserLoggedIn] = useState('USER_OFFLINE');
+    const [userLoggedIn, setUserLoggedIn] = useState('');
+    const { user } = useContext(userContext)
 
     const qs = new QuickSort();
 
     const [itemsSections, setItemsSections] = useState(null);
 
     useEffect(() => {
+        if(user === null) {
+            setUserLoggedIn('USER_OFFLINE');
+        }else{
+            setUserLoggedIn('USER_ONLINE');
+        }
+    }, [user])
+
+    useEffect(() => {
         UserDAO.getUserTags(1, (tagViewCount) => {
-            // console.log(tagViewCount)
+
             const sortedArray = qs.reverseSort(tagViewCount, "nbCount");
-            // console.log(sortedArray)
-            const tempArray = sortedArray.map((item) => {
+            const sortedArray5 = sortedArray.slice(0, 5);
+            const tempArray = sortedArray5.map((item) => {
                 return <ItemSection sectionName={item.id_tag} sectionLink={'https://www.google.com'} bottomSpacing={20} containerHeight={300} imageSize={200} key={item.id_tag}/>
             });
             
             setItemsSections(tempArray);
         });
     }, [userLoggedIn]);
+
+    
 
     return (
         <div className='page'>
--- dev/app/src/component/ItemSection.jsx
+++ dev/app/src/component/ItemSection.jsx
@@ -4,107 +4,131 @@
 // Autre auteurs: Nathaelle Fournier
 // Date : Hiver 2023
 
-import React, { useContext} from "react";
+import React, { useContext, useEffect, useState} from "react";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import ItemTag from './ItemTag';
-import { paletteContext } from "./Context";
+import { paletteContext, tagsContext } from "./Context";
+import { ItemDAO } from "../DAO/ItemDAO";
 
 export default function ItemSection(props){
 
     const {palette} = useContext(paletteContext)
+    const {tags} = useContext(tagsContext)
+    const [items, setItems] = useState([])
 
-    //AU LIEU DE TEST UTILISER APPEL API
-    const test= [
-        {   
-            id: 1,
-            name: 'Barbie',
-            price: 15.00,
-            date: '10-10-21',
-            img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
-            link: 'https://www.google.com'
-        },
-        {
-            id: 2,
-            name: 'Doggo',
-            price: 15.00,
-            date: '10-10-21',
-            img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
-            link: 'https://www.google.com'
-        },
-        {
-            id: 3,
-            name: 'Doggo',
-            price: 15.00,
-            date: '10-10-21',
-            img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
-            link: 'https://www.google.com'
-        },
-        {
-            id: 4,
-            name: 'Doggo',
-            price: 15.00,
-            date: '10-10-21',
-            img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
-            link: 'https://www.google.com'
-        },
-        {
-            id: 5,
-            name: 'Doggo',
-            price: 15.00,
-            date: '10-10-21',
-            img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
-            link: 'https://www.google.com'
-        },
-        {
-            id: 6,
-            name: 'Doggo',
-            price: 15.00,
-            date: '10-10-21',
-            img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
-            link: 'https://www.google.com'
-        },
-        {
-            id: 7,
-            name: 'Doggo',
-            price: 15.00,
-            date: '10-10-21',
-            img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
-            link: 'https://www.google.com'
-        },
-        {
-            id: 8,
-            name: 'Doggo',
-            price: 15.00,
-            date: '10-10-21',
-            img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
-            link: 'https://www.google.com'
-        },
-        {
-            id: 9,
-            name: 'Doggo',
-            price: 15.00,
-            date: '10-10-21',
-            img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
-            link: 'https://www.google.com'
-        }
-    ]
+    // AU LIEU DE TEST UTILISER APPEL API
+    // const test= [
+    //     {   
+    //         id: 1,
+    //         name: 'Barbie',
+    //         price: 15.00,
+    //         date: '10-10-21',
+    //         img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
+    //         link: 'https://www.google.com'
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'Doggo',
+    //         price: 15.00,
+    //         date: '10-10-21',
+    //         img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
+    //         link: 'https://www.google.com'
+    //     },
+    //     {
+    //         id: 3,
+    //         name: 'Doggo',
+    //         price: 15.00,
+    //         date: '10-10-21',
+    //         img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
+    //         link: 'https://www.google.com'
+    //     },
+    //     {
+    //         id: 4,
+    //         name: 'Doggo',
+    //         price: 15.00,
+    //         date: '10-10-21',
+    //         img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
+    //         link: 'https://www.google.com'
+    //     },
+    //     {
+    //         id: 5,
+    //         name: 'Doggo',
+    //         price: 15.00,
+    //         date: '10-10-21',
+    //         img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
+    //         link: 'https://www.google.com'
+    //     },
+    //     {
+    //         id: 6,
+    //         name: 'Doggo',
+    //         price: 15.00,
+    //         date: '10-10-21',
+    //         img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
+    //         link: 'https://www.google.com'
+    //     },
+    //     {
+    //         id: 7,
+    //         name: 'Doggo',
+    //         price: 15.00,
+    //         date: '10-10-21',
+    //         img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
+    //         link: 'https://www.google.com'
+    //     },
+    //     {
+    //         id: 8,
+    //         name: 'Doggo',
+    //         price: 15.00,
+    //         date: '10-10-21',
+    //         img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
+    //         link: 'https://www.google.com'
+    //     },
+    //     {
+    //         id: 9,
+    //         name: 'Doggo',
+    //         price: 15.00,
+    //         date: '10-10-21',
+    //         img: `https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`,
+    //         link: 'https://www.google.com'
+    //     }
+    // ]
 
-    const items = test.map(
-        (item) => {
-            return(
-                    <ItemTag 
-                    key={item.id} 
-                    img={item.img} 
-                    name={item.name} 
-                    price={item.price} 
-                    date={item.date} 
-                    imageSize={props.imageSize} 
-                    link={item.link}
-                    />
-            )
+    useEffect(() => {
+        if (tags.includes(props.sectionName)) {
+            ItemDAO.getItemsByKeyTag(props.sectionName, (result) => {
+                const mappedItems = result.map((element) => {
+                    return (
+                        <ItemTag
+                        key={element.id}
+                        img={`https://picsum.photos/id/${Math.floor(Math.random() * 500)}/200`}
+                        name={element.name}
+                        price={parseFloat(element.price)}
+                        date={new Date(element.auction_on).toLocaleDateString()}
+                        imageSize={props.imageSize}
+                        link={element.link}
+                        />
+                    );
+                });
+                setItems(mappedItems);
+            });
         }
-    )
+    }, [props.sectionName, tags])
+
+    // const items = test.map(
+    //     (item) => {
+    //         return(
+    //                 <ItemTag 
+    //                 key={item.id} 
+    //                 img={item.img} 
+    //                 name={item.name} 
+    //                 price={item.price} 
+    //                 date={item.date} 
+    //                 imageSize={props.imageSize} 
+    //                 link={item.link}
+    //                 />
+    //         )
+    //     }
+    // )
 
     const responsive = {
         superLargeDesktop: {
