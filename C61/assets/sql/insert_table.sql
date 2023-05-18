8b3c7b95b36db4d0ec57938fd11de26a04008951
Nathaelle Fournier <nathaellefournier@gmail.com>
2023-05-18 14:01:26 -0400

    Mise a jour insertion

--- C61/assets/sql/insert_table.sql
+++ C61/assets/sql/insert_table.sql
@@ -3,10 +3,19 @@
 INSERT INTO users VALUES (DEFAULT, 'dajohn', 'John', 'Smith', 'johnsmith@email.com', 'AAAaaa111', DEFAULT, CURRENT_TIMESTAMP);
 INSERT INTO items VALUES (DEFAULT, '1980-ish Barbie', 'The first one ever WOW!', DEFAULT, DEFAULT, 8999.99, 1, '2023-04-26',1);
 INSERT INTO items VALUES (DEFAULT, 'LOZ:BOTW2', 'The wait is almost over!', DEFAULT, DEFAULT, 89.99, 1, '2023-05-15',1);
+INSERT INTO items VALUES (DEFAULT, 'DEC Certificate', 'I want to break free', DEFAULT, DEFAULT, 280.99, 1, '2023-05-27',1);
+
+INSERT INTO tag_list VALUES (3, 'Livre & Manuscrit');
+INSERT INTO tag_list VALUES (3, 'Bijoux & Accessoire');
+INSERT INTO tag_list VALUES (1, 'Collection');
+INSERT INTO tag_list VALUES (2, 'Collection');
+INSERT INTO tag_list VALUES (3, 'Art & Sculture');
 
 SELECT * FROM items;
 SELECT * FROM users;
 
+
+-- Requete
 SELECT *,
 ( items.auction_on - CURRENT_TIMESTAMP) AS "time_idx" 
 FROM items 
