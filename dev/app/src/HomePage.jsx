cac909415917f3704fedac0e8f613b36c8cfe717
QHT <quoc.huan.tran@gmail.com>
2023-05-18 17:41:13 -0400

    Making home for offline user

--- dev/app/src/HomePage.jsx
+++ dev/app/src/HomePage.jsx
@@ -3,6 +3,7 @@ import ItemSection from './component/ItemSection';
 import { UserDAO } from './DAO/UserDAO';
 import QuickSort from './assets/QuickSort';
 import { userContext } from './component/Context';
+import { ItemDAO } from './DAO/ItemDAO';
 
 export default function HomePage(props) {
 
@@ -18,26 +19,40 @@ export default function HomePage(props) {
 
     const [itemsSections, setItemsSections] = useState(null);
 
+    // useEffect(() => {
+    //     if(user === null) {
+    //         setUserLoggedIn('USER_OFFLINE');
+    //     }else{
+    //         setUserLoggedIn('USER_ONLINE');
+    //     }
+    // }, [user])
+
+
     useEffect(() => {
+        
         if(user === null) {
-            setUserLoggedIn('USER_OFFLINE');
+            ItemDAO.getFavoriteTags(1, (tagViewCount) => {
+                console.log(tagViewCount)
+                const sortedArray = qs.reverseSort(tagViewCount, "nbCount");
+                const sortedArray5 = sortedArray.slice(0, 5);
+                const tempArray = sortedArray5.map((item) => {
+                    return <ItemSection sectionName={item.id_tag} sectionLink={'https://www.google.com'} bottomSpacing={20} containerHeight={300} imageSize={200} key={item.id_tag}/>
+                });
+                
+                setItemsSections(tempArray);
+            })
         }else{
-            setUserLoggedIn('USER_ONLINE');
-        }
-    }, [user])
-
-    useEffect(() => {
-        UserDAO.getUserTags(1, (tagViewCount) => {
-
-            const sortedArray = qs.reverseSort(tagViewCount, "nbCount");
-            const sortedArray5 = sortedArray.slice(0, 5);
-            const tempArray = sortedArray5.map((item) => {
-                return <ItemSection sectionName={item.id_tag} sectionLink={'https://www.google.com'} bottomSpacing={20} containerHeight={300} imageSize={200} key={item.id_tag}/>
+            UserDAO.getUserTags(1, (tagViewCount) => {
+                const sortedArray = qs.reverseSort(tagViewCount, "nbCount");
+                const sortedArray5 = sortedArray.slice(0, 5);
+                const tempArray = sortedArray5.map((item) => {
+                    return <ItemSection sectionName={item.id_tag} sectionLink={'https://www.google.com'} bottomSpacing={20} containerHeight={300} imageSize={200} key={item.id_tag}/>
+                });
+        
+                setItemsSections(tempArray);
             });
-            
-            setItemsSections(tempArray);
-        });
-    }, [userLoggedIn]);
+        }
+    }, [userLoggedIn, user]);
 
     
 
